dfb84cd4da59072c6cbf82f4e0b0b8ed46f9a3af
perezyuan <yzmspirit@sina.cn>
2016-12-14 14:37:49 +0800

    change layout and style

--- routes/japan.js
+++ routes/japan.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('japan.html', {});
+    res.render('japan.html', {
+        title : 'Love in Japan'
+    });
 });
 
 module.exports = router;
