aa7bb3f2db06bbe4daf3c6e9dfa6adeed3fb9a6c
perezyuan <yzmspirit@sina.cn>
2016-12-14 17:26:46 +0800

    add fav

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ nunjucks.configure(path.join(__dirname, 'views'), {
   watch: true
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/src', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
