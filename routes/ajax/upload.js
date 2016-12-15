62089f439757ffcaf8ec8528905901d3d6860342
perezyuan <yzmspirit@sina.cn>
2016-12-15 15:20:07 +0800

    make upload

--- routes/ajax/upload.js
+++ routes/ajax/upload.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multiparty = require('connect-multiparty');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
 
-/* GET home page. */
-router.post('/', function (req, res, next) {
-    console.log(req.query);
-    res.json({code : 0});
-});
+let uploadDir = path.join(__dirname + '/temp/');
+let multipartMiddleware = multiparty({ uploadDir });
+router.post('/', multipartMiddleware, (req, res, next) => {
+    _.forEach(req.files, (value, key) => {
+        fs.rename(value.path, uploadDir + value.originalFilename, (err) => {
+            if (err) {
+                console.log('rename error: ' + err);
+            } else {
+                console.log('rename ok');
+            }
+        });
+    });
+    res.json({code: 200})
+})
 
 module.exports = router;
