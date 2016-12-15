df8baa98591353b3617bbc0c9cd00ffabd6cd284
perezyuan <yzmspirit@sina.cn>
2016-12-15 15:32:27 +0800

    change temp path

--- routes/ajax/upload.js
+++ routes/ajax/upload.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 
-let uploadDir = path.join(__dirname + '/temp/');
+let uploadDir = path.resolve(__dirname, '../../temp');
 let multipartMiddleware = multiparty({ uploadDir });
 router.post('/', multipartMiddleware, (req, res, next) => {
     _.forEach(req.files, (value, key) => {
-        fs.rename(value.path, uploadDir + value.originalFilename, (err) => {
+        fs.rename(value.path, uploadDir + '/' + value.originalFilename, (err) => {
             if (err) {
                 console.log('rename error: ' + err);
             } else {
