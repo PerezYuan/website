67e453d0b5a40e54eeb05ae424421ca838cb8b49
perezyuan <yzmspirit@sina.cn>
2017-01-05 11:08:44 +0800

    add mysql config

--- conf/conf.js
+++ conf/conf.js
@@ -11,7 +11,9 @@ let config = {
     mysql: {
         host: '127.0.0.1',
         user: 'root',
-        password: '***'
+        password: '***',
+        port: 3306,
+        database: 'excel',
     }
 }
 
