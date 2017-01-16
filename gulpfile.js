d3746bce3b3fdb788f730f9d3f90fdae59a533fb
perezyuan <yzmspirit@sina.cn>
2017-01-16 15:09:09 +0800

    add gulp-image and do some update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     less = require('gulp-less'),
     webpack = require('gulp-webpack'),
+    image = require('gulp-image'),
     del = require('del');
 
 
@@ -10,13 +11,24 @@ gulp.task('jsmin', function() {
         .pipe(gulp.dest('public/dist/js'))
 });
 
+gulp.task('image', function () {
+    return gulp.src('public/src/images/**')
+        .pipe(image())
+        .pipe(gulp.dest('public/dist/images'));
+});
+
 gulp.task('less', function () {
     return gulp.src('public/src/css/*.less')
         .pipe(less())
         .pipe(gulp.dest('public/dist/css'));
 });
 
-gulp.task('default', ['jsmin', 'less'], function() {});
+gulp.task('font', function() {
+    return gulp.src('public/src/css/fonts/**')
+        .pipe(gulp.dest('public/dist/css/fonts'))
+})
+
+gulp.task('default', ['jsmin','image', 'less', 'font'], function() {});
 
 gulp.watch('public/src/js/*.js', function(event) {
     gulp.run('jsmin');
