9e94b091a5a356caa258c1f1bd611a9aba167039
perezyuan <yzmspirit@sina.cn>
2017-01-18 17:42:48 +0800

    data bug

--- public/dist/js/japan.js
+++ public/dist/js/japan.js
@@ -156,6 +156,11 @@
 	        var fd = new FormData(),
 	            me = this,
 	            files = me.$el[0].files;
+	        if (Object.prototype.toString.call(me.config.beforeSend) === '[object Function]') {
+	            if (me.config.beforeSend(me.$el) === false) {
+	                return false;
+	            }
+	        }
 	        for (var i = 0, ii = files.length; i < ii; i++) {
 	            // 判断文件大小
 	            if (files[i].size > me.config.maxSize * 1024 * 1024) {
@@ -167,29 +172,24 @@
 	                alert("暂不支持该类型的文件，请重新选择!");
 	                return false;
 	            }
-	            if (Object.prototype.toString.call(me.config.beforeSend) === '[object Function]') {
-	                if (me.config.beforeSend(me.$el) === false) {
-	                    return false;
+	            fd.append('file' + i, files[i]);
+	        }
+	        $.ajax({
+	            type: "post",
+	            url: "/ajax/upload",
+	            data: fd,
+	            // 取消数据预处理
+	            processData: false,
+	            // 自动加上正确的Content-Type
+	            contentType: false,
+	            xhr: function xhr() {
+	                var xhr = $.ajaxSettings.xhr();
+	                if (Object.prototype.toString.call(me.config.onProgress) === '[object Function]' && xhr.upload) {
+	                    xhr.upload.addEventListener("progress", me.config.onProgress, false);
+	                    return xhr;
 	                }
 	            }
-	            fd.append('file' + i, files[i]);
-	            $.ajax({
-	                type: "post",
-	                url: "/ajax/upload",
-	                data: fd,
-	                // 取消数据预处理
-	                processData: false,
-	                // 自动加上正确的Content-Type
-	                contentType: false,
-	                xhr: function xhr() {
-	                    var xhr = $.ajaxSettings.xhr();
-	                    if (Object.prototype.toString.call(me.config.onProgress) === '[object Function]' && xhr.upload) {
-	                        xhr.upload.addEventListener("progress", me.config.onProgress, false);
-	                        return xhr;
-	                    }
-	                }
-	            });
-	        }
+	        });
 	    }
 	};
 	
--- public/src/js/util/uploader.js
+++ public/src/js/util/uploader.js
@@ -52,6 +52,11 @@ uploader.prototype = {
         let fd = new FormData,
             me = this,
             files = me.$el[0].files;
+        if (Object.prototype.toString.call(me.config.beforeSend) === '[object Function]') {
+            if (me.config.beforeSend(me.$el) === false) {
+                return false;
+            }
+        }
         for (let i = 0, ii = files.length; i < ii; i++) {
             // 判断文件大小
             if (files[i].size > me.config.maxSize * 1024 * 1024) {
@@ -62,31 +67,26 @@ uploader.prototype = {
             if (me.config.type.indexOf(type.toLocaleLowerCase()) == -1) {
                 alert("暂不支持该类型的文件，请重新选择!");
                 return false;
-            }
-            if (Object.prototype.toString.call(me.config.beforeSend) === '[object Function]') {
-                if (me.config.beforeSend(me.$el) === false) {
-                    return false;
-                }
-            }
+            }            
             fd.append('file' + i, files[i]);
-            $.ajax({
-                type: "post",
-                url: "/ajax/upload",
-                data: fd,
-                // 取消数据预处理
-                processData: false,
-                // 自动加上正确的Content-Type
-                contentType: false,
-                xhr: function () {
-                    let xhr = $.ajaxSettings.xhr();
-                    if (Object.prototype.toString.call(me.config.onProgress) === '[object Function]'
-                        && xhr.upload) {
-                        xhr.upload.addEventListener("progress", me.config.onProgress, false);
-                        return xhr;
-                    }
-                }
-            });
         }
+        $.ajax({
+            type: "post",
+            url: "/ajax/upload",
+            data: fd,
+            // 取消数据预处理
+            processData: false,
+            // 自动加上正确的Content-Type
+            contentType: false,
+            xhr: function () {
+                let xhr = $.ajaxSettings.xhr();
+                if (Object.prototype.toString.call(me.config.onProgress) === '[object Function]'
+                    && xhr.upload) {
+                    xhr.upload.addEventListener("progress", me.config.onProgress, false);
+                    return xhr;
+                }
+            }
+        });
     }
 }
 
